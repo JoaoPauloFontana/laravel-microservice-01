f6be4661d3d733d37ce02f15bc4038e7b8204b91
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2023-02-24 23:32:56 -0300

    :test_tube: tests: adicionado testes

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::factory()->create(),
+            'name' => $this->faker->unique()->name(),
+            'email' => $this->faker->unique()->email(),
+            'whatsapp' => $this->faker->unique()->numberBetween(1000000000, 99999999999),
+        ];
+    }
+}
